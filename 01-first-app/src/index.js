88005ce1e488442ca632736567099bbfbb51cc71
M D Hossain <delwarbscse@gmail.com>
2023-01-14 12:16:32 +0600

    React JSX

--- 01-first-app/src/index.js
+++ 01-first-app/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const date = new Date();
+const getDay = date.getDate();
+const getMonth = date.getMonth();
+const getYear = date.getFullYear();
+
+const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>M D Hossain</h1>
+    <div>
+      <h1>Lorem Ipsum</h1>
+      <h2>{getDay+"/"+getMonth+"/"+getYear}</h2>
+      <p>{loremIpsum}</p>
+    </div>
 );
