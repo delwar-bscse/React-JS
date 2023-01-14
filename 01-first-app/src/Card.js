e1d7a6e76fd2593367789336f3f60d932a34ce3e
M D Hossain <delwarbscse@gmail.com>
2023-01-14 20:03:44 +0600

    External functional Component

---
+++ 01-first-app/src/Card.js
@@ -0,0 +1,18 @@
+
+const date = new Date();
+const getDay = date.getDate();
+const getMonth = date.getMonth();
+const getYear = date.getFullYear();
+
+const todoTitle = "Call Family"
+const todoDes = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+function Card() {
+  return  <div className='card'>
+            <h3 className='cardTitle'>{todoTitle}</h3>
+            <p className='todoDes'>{todoDes}</p>
+            <p className='todoFooter'>{getDay+"/"+getMonth+"/"+getYear}</p>
+          </div>
+}
+
+export default Card;
--- 01-first-app/src/index.js
+++ 01-first-app/src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-
-const date = new Date();
-const getDay = date.getDate();
-const getMonth = date.getMonth();
-const getYear = date.getFullYear();
-
-const todoTitle = "Call Family"
-const todoDes = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-
-function Card() {
-  return  <div className='card'>
-            <h3 className='cardTitle'>{todoTitle}</h3>
-            <p className='todoDes'>{todoDes}</p>
-            <p className='todoFooter'>{getDay+"/"+getMonth+"/"+getYear}</p>
-          </div>
-}
+import Card from './Card.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
