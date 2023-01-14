c664b97e542c362fe3f89231cdab8582637e4f20
M D Hossain <delwarbscse@gmail.com>
2023-01-14 19:55:43 +0600

    Create functional Component

--- 01-first-app/src/index.js
+++ 01-first-app/src/index.js
@@ -10,15 +10,21 @@ const getYear = date.getFullYear();
 const todoTitle = "Call Family"
 const todoDes = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
+function Card() {
+  return  <div className='card'>
+            <h3 className='cardTitle'>{todoTitle}</h3>
+            <p className='todoDes'>{todoDes}</p>
+            <p className='todoFooter'>{getDay+"/"+getMonth+"/"+getYear}</p>
+          </div>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
       <h1 className='headingStyle'>Todo App</h1>
-      <div className='card'>
-        <h3 className='cardTitle'>{todoTitle}</h3>
-        <p className='todoDes'>{todoDes}</p>
-        <p className='todoFooter'>{getDay+"/"+getMonth+"/"+getYear}</p>
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </div>
 );
